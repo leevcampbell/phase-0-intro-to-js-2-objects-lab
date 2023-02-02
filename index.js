a45301e32c59bc5fe7c6f51519fc5e83ee3dd1f2
LeeVincentCampbell <leevcampbell@gmail.com>
2023-02-01 20:31:05 -0500

    inputted object functions into index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+} ;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value ;
+    return updatedEmployee ;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value ;
+    return employee ;
+} ;
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee, key} ;
+    delete newEmployee[key] ;
+    return newEmployee ;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key] ;
+    return employee ;
+};
